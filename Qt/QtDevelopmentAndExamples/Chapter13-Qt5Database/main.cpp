ef52afdf5bfbd35f143e44edfe653bc421c301e2
Guido <15823159025@163.com>
2021-08-06 11:55:21 +0800

    完成 mysql 连接

--- Qt/QtDevelopmentAndExamples/Chapter13-Qt5Database/main.cpp
+++ Qt/QtDevelopmentAndExamples/Chapter13-Qt5Database/main.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 #include <QSqlDatabase>
@@ -15,69 +15,79 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
 
-    /*
-    //添加数据库驱动
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    //设置数据库名称
-    db.setDatabaseName(":memory:");
-    //打开数据库
-    if(!db.open())
     {
-        return false;
+        //添加数据库驱动
+        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+        //设置数据库名称
+        db.setDatabaseName(":memory:");
+        //打开数据库
+        if(!db.open())
+        {
+            return false;
+        }
+
+        //以下执行相关sql语句
+        QSqlQuery query;
+
+        //新建student表，id设置为主键，还有一个name项
+        query.exec("create table student(id int primary key,name varchar)");
+
+        //向表中插入3条记录
+        query.exec("insert into student values(1,'xiaogang')");
+        query.exec("insert into student values(2,'xiaoming')");
+        query.exec("insert into student values(3,'xiaohong')");
+
+        //查找表中id >=2 的记录的id项和name项的值
+        query.exec("select id,name from student where id >= 2");
+
+        //query.next()指向查找到的第一条记录，然后每次后移一条记录
+        while(query.next())
+        {
+            //query.value(0)是id的值，将其转换为int型
+            int value0 = query.value(0).toInt();
+            QString value1 = query.value(1).toString();
+            //输出两个值
+            qDebug() << value0 << value1 ;
+        }
+
+        db.close();
+
     }
 
-    //以下执行相关sql语句
-    QSqlQuery query;
-
-    //新建student表，id设置为主键，还有一个name项
-    query.exec("create table student(id int primary key,name varchar)");
-
-    //向表中插入3条记录
-    query.exec("insert into student values(1,'xiaogang')");
-    query.exec("insert into student values(2,'xiaoming')");
-    query.exec("insert into student values(3,'xiaohong')");
-
-    //查找表中id >=2 的记录的id项和name项的值
-    query.exec("select id,name from student where id >= 2");
-
-    //query.next()指向查找到的第一条记录，然后每次后移一条记录
-    while(query.next())
-    {
-        //query.value(0)是id的值，将其转换为int型
-        int value0 = query.value(0).toInt();
-        QString value1 = query.value(1).toString();
-        //输出两个值
-        qDebug() << value0 << value1 ;
-    }
-
-    db.close();
-    //QSqlDatabase::removeDatabase(":memory:");
+#if 0
+    QSqlDatabase::removeDatabase(":memory:");
+#else
     QString name;
     {
         name = QSqlDatabase::database().connectionName();
     }//超出作用域，隐含对象QSqlDatabase::database()被删除。
     QSqlDatabase::removeDatabase(name);
+#endif
 
-    */
 
-    QSqlDatabase staticecgdb = QSqlDatabase::addDatabase("QMYSQL");
-    staticecgdb.setHostName("");
+    QSqlDatabase staticecgdb = QSqlDatabase::addDatabase("QMYSQL", "MySQL_alan");
+    staticecgdb.setHostName("localhost");
     staticecgdb.setPort(3306);
-    staticecgdb.setDatabaseName("testdatabase");
+    staticecgdb.setDatabaseName("alan");
     staticecgdb.setUserName("root");
     staticecgdb.setPassword("");
 
     if(staticecgdb.open())
     {
         QSqlTableModel *model = new QSqlTableModel( nullptr, staticecgdb);
-        model->setTable("center");
+        model->setTable("userinfo");
         model->setEditStrategy(QSqlTableModel::OnManualSubmit);
-        model->select();
-        model->setHeaderData(0, Qt::Horizontal, "Id");
-        model->setHeaderData(1, Qt::Horizontal, "Name");
-        QTableView *view = new QTableView;
-        view->setModel(model);
-        view->show();
+
+        if(model->select())
+        {
+            model->setHeaderData(0, Qt::Horizontal, "id");
+            model->setHeaderData(1, Qt::Horizontal, "username");
+            model->setHeaderData(1, Qt::Horizontal, "password");
+
+            QTableView *view = new QTableView;
+            view->setModel(model);
+            view->show();
+        }
     }
 
     return a.exec();
